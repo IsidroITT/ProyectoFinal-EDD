f3b67b3ff6358e6084b5aa108ccff4de52230740
IsidroITT <isanfloresfa@ittepic.edu.mx>
2022-05-07 16:12:25 -0600

    Metodos de ordenamiento

--- src/MetodosDeBusqueda/Busqueda.java
+++ src/MetodosDeBusqueda/Busqueda.java
@@ -1,21 +1,53 @@
 /* Metodos de busqueda
-- Busqueda secuencial
-- Busqueda binaria
+- Busqueda secuencial -L
+- Busqueda binaria        -L
 */
 package MetodosDeBusqueda;
+
+import MetodosDeOrdenamiento.Ordenamiento;
 
 /**
  *
  * @author isidro
  */
 public class Busqueda {
-    public Busqueda(){}
+    Ordenamiento BB;
+    int pos;
     
-    public void Secuencial(long[] vector){
-        
+    public Busqueda(){
+        pos = 0;
+        BB = new Ordenamiento();
     }
     
-    public void Binaria(long[] vector){
+    public boolean Secuencial(long[] vector, long dato){
+        for (int i = 0; i < vector.length-1; i++) {
+            if(vector[i] == dato){
+                pos = i;
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean Binaria(long[] vectorOrdenado, long dato){
+        int centro, primero, ultimo;
+        long valorCentro;
         
+        primero = 0;
+        ultimo = vectorOrdenado.length-1;
+        
+        while (primero <= ultimo) {
+            centro = (primero + ultimo) / 2;
+            valorCentro = vectorOrdenado[centro];
+            if (dato == valorCentro) {
+                pos = centro;
+                return true;
+            } else if (dato < valorCentro) {
+                ultimo = centro - 1;
+            } else {
+                primero = centro + 1;
+            }
+        }
+        return false;
     }
 }
